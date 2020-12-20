884e61f96820a0a061788fd1157a0f2c2e8c17c4
Akash Dubey <akashrdubey19@gmail.com>
2020-12-20 16:44:54 +0530

    login Page UI complete

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -43,128 +43,132 @@ class _LoginPageState extends State<LoginPage> {
         body: Form(
           key: _formKey,
           child: Container(
+            alignment: Alignment.bottomCenter,
             padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 15.0),
             decoration: BoxDecoration(
               gradient: backgroundGradient,
             ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  width: double.infinity,
-                  child: Image.asset(
-                    "images/login.png",
-                    fit: BoxFit.contain,
-                    height: AppMediaQuery(context).appHeight(35.0),
-                    alignment: Alignment.bottomRight,
-                  ),
-                ),
-                Text(
-                  "Login",
-                  style: mainHeader,
-                ),
-                SizedBox(
-                  height: AppMediaQuery(context).appHeight(02),
-                ),
-                TextFormField(
-                  decoration: InputDecoration(
-                    prefixIcon: Icon(Icons.snooze),
-                    border: OutlineInputBorder(
-                      borderSide: textFieldBorderSide,
-                      borderRadius: textFieldBorderRadius,
-                    ),
-                    labelText: 'Email-id',
-                    labelStyle: hintStyle,
-                  ),
-                  keyboardType: TextInputType.emailAddress,
-                  validator: (value) {
-                    if (!validateEmail(value)) {
-                      return "Please Enter a valid Email!";
-                    }
-                    return null;
-                  },
-                  onSaved: (newValue) {
-                    _userEmail = newValue;
-                  },
-                ),
-                SizedBox(
-                  height: 24,
-                ),
-                TextFormField(
-                  decoration: InputDecoration(
-                    prefixIcon: Icon(Icons.snooze),
-                    border: OutlineInputBorder(
-                      borderSide: textFieldBorderSide,
-                      borderRadius: textFieldBorderRadius,
-                    ),
-                    suffixIcon: IconButton(
-                      icon: Icon(Icons.panorama_fish_eye),
-                      onPressed: () {
-                        setState(() {
-                          showPassword = !showPassword;
-                        });
-                      },
-                    ),
-                    labelText: 'Password',
-                    labelStyle: hintStyle,
-                  ),
-                  obscureText: showPassword,
-                  keyboardType: TextInputType.text,
-                  validator: (value) {
-                    if (value.isEmpty || value.length < 6) {
-                      return "Password should be greater then 6 letter";
-                    }
-                    return null;
-                  },
-                  onSaved: (newValue) {
-                    _userPassword = newValue;
-                  },
-                ),
-                SizedBox(
-                  height: 5.0,
-                ),
-                Container(
-                  alignment: Alignment.centerRight,
-                  child: InkWell(child: Text("forgot-password")),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Container(
-                  padding: EdgeInsets.symmetric(horizontal: 20.0),
-                  width: double.infinity,
-                  child: RaisedButton(
-                    padding: EdgeInsets.symmetric(vertical: 16.0),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(16.0),
-                    ),
-                    child: Text(
-                      "Login",
-                      style: TextStyle(fontSize: 16.0),
-                    ),
-                    onPressed: _trySubmit,
-                  ),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Divider(
-                  color: Colors.white,
-                  height: 28,
-                  thickness: 1,
-                  indent: 0,
-                  endIndent: 0,
-                ),
-                Container(
-                  alignment: Alignment.center,
-                  child: InkWell(
-                    child: Text(
-                      "Don't have Acc, Sign-Up!",
-                      style: TextStyle(fontSize: 16.0, color: Colors.white),
+            child: Container(
+              height: AppMediaQuery(context).appHeight(90.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    width: double.infinity,
+                    child: Image.asset(
+                      "images/login.png",
+                      fit: BoxFit.contain,
+                      height: AppMediaQuery(context).appHeight(35.0),
+                      alignment: Alignment.bottomRight,
                     ),
                   ),
-                ),
-              ],
+                  Text(
+                    "Login",
+                    style: mainHeader,
+                  ),
+                  SizedBox(
+                    height: AppMediaQuery(context).appHeight(02),
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.snooze),
+                      border: OutlineInputBorder(
+                        borderSide: textFieldBorderSide,
+                        borderRadius: textFieldBorderRadius,
+                      ),
+                      labelText: 'Email-id',
+                      labelStyle: hintStyle,
+                    ),
+                    keyboardType: TextInputType.emailAddress,
+                    validator: (value) {
+                      if (!validateEmail(value)) {
+                        return "Please Enter a valid Email!";
+                      }
+                      return null;
+                    },
+                    onSaved: (newValue) {
+                      _userEmail = newValue;
+                    },
+                  ),
+                  SizedBox(
+                    height: 24,
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.snooze),
+                      border: OutlineInputBorder(
+                        borderSide: textFieldBorderSide,
+                        borderRadius: textFieldBorderRadius,
+                      ),
+                      suffixIcon: IconButton(
+                        icon: Icon(Icons.panorama_fish_eye),
+                        onPressed: () {
+                          setState(() {
+                            showPassword = !showPassword;
+                          });
+                        },
+                      ),
+                      labelText: 'Password',
+                      labelStyle: hintStyle,
+                    ),
+                    obscureText: showPassword,
+                    keyboardType: TextInputType.text,
+                    validator: (value) {
+                      if (value.isEmpty || value.length < 6) {
+                        return "Password should be greater then 6 letter";
+                      }
+                      return null;
+                    },
+                    onSaved: (newValue) {
+                      _userPassword = newValue;
+                    },
+                  ),
+                  SizedBox(
+                    height: 5.0,
+                  ),
+                  Container(
+                    alignment: Alignment.centerRight,
+                    child: InkWell(child: Text("forgot-password")),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 20.0),
+                    width: double.infinity,
+                    child: RaisedButton(
+                      padding: EdgeInsets.symmetric(vertical: 16.0),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(16.0),
+                      ),
+                      child: Text(
+                        "Login",
+                        style: TextStyle(fontSize: 16.0),
+                      ),
+                      onPressed: _trySubmit,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Divider(
+                    color: Colors.white,
+                    height: 28,
+                    thickness: 1,
+                    indent: 0,
+                    endIndent: 0,
+                  ),
+                  Container(
+                    alignment: Alignment.center,
+                    child: InkWell(
+                      child: Text(
+                        "Don't have Acc, Sign-Up!",
+                        style: TextStyle(fontSize: 16.0, color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
