6abc2ee84e59706a1133b8e97f1ebcd80117ceef
Akash Dubey <akashrdubey19@gmail.com>
2021-01-03 20:02:13 +0530

    app Icons added

--- lib/Utils/dateUtil.dart
+++ lib/Utils/dateUtil.dart
@@ -5,5 +5,7 @@ class DateUtil {
 
   dateformatymd(val) => DateFormat('yyyy-MM-dd').format(val);
 
-  dateformatDefault(val) => DateFormat.yMMMMd().format(val);
+  dateformatDefault1(val) => DateFormat().format(val);
+  // dateformatDefault(val) => DateFormat("kk:mm:a").add_MMMMEEEEd().format(val);
+  dateformatDefault(val) => DateFormat("MMM, d/y -").add_jm().format(val);
 }
